c4506ee5fe71d514f25db9d174303cf3f6c50ce6
FranciscoTavaress <franciscoduartetav@gmail.com>
2024-11-26 14:08:51 +0000

    mudanças

--- exercicio5/mainEx5.c
+++ exercicio5/mainEx5.c
@@ -4,50 +4,91 @@
 #include <jansson.h>    // Para trabalhar com JSON
 #include <curl/curl.h>  // Para usar a biblioteca libcurl, que é usada na função get_uri
 
-#include "help.h"
-#include "list.h"
 
-typedef struct {
-        int id;
-        float price;
-        const char *description;
-        const char *category;
-} Product;
+#include "../exercicio3/users.h"
+#include "../exercicio3/products.h"
+#include "../exercicio3/cart.h"
 
-typedef struct {
-    int id;
-    const char *name;
-} User;
+Cart *create_cart(size_t n_products){
+    Cart *cart = malloc(sizeof(Cart) + sizeof(cart->products[0]) * n_products);
 
-typedef struct {
-    int user_id;
-    size_t n_products;
-    struct {
-        int id;
-        size_t quantity;
-    } products[];
-} Cart;
+    cart->user_id = -1;
+    cart->n_products = n_products;
 
-typedef struct {
-    Node *head;
-} Users;
-
-typedef struct{
-    Node *head;
-} Products;
-
-//------------------------------------------------------------//
-
-void list_users() { 
-    Users *users = users_get();
-    for(Node *head = Users->head; head != NULL; head = head->next){
-        User *data = (User *)head->data;
-        printf("%s\n", data->name);
+    for (size_t i = 0; i < n_products; i++) {
+        cart->products[i].id = 0;
+        cart->products[i].quantity = 0;
     }
+
+    return cart;
+}
+void list_users(Users *users) { 
+    printf("\n");
+    Node *new_head = users->head;
+    for(Node *user = new_head->next; user != NULL; user = user->next){
+        User* data = (User *)user->data;
+        if(data != NULL){
+            printf("%s\n", data->name);
+        }
+    }
+    printf("\n");
+}
+
+void identify_user(char* input_id, Cart *cart){
+    printf("\n");
+    int id = atoi(input_id);
+    if(id == 0){
+        printf("Error\n");
+    } else {
+        cart->user_id = id;
+        printf("Carrinho com ID %d inicializado\n", id);
+    }
+    printf("\n");
+}
+
+void list_specified_products(char* input_category, char *critery, Products *products){
+    printf("\n");
+    int category_chosed = false; // flag 
+    if (strcmp(critery, "<") != 0 && strcmp(critery, ">") != 0) {
+        printf("Erro: critério inválido. Use '<' ou '>'.\n");
+        return; 
+    }
+    Node *new_head = products->head;
+    for(Node *product = new_head->next; product != NULL; product = product->next){
+        Product* data = (Product *)product->data;
+        if(strcmp(data->category, input_category) == 0){
+            category_chosed = true;
+            break;
+        }
+    }
+    if(!category_chosed){
+        printf("Nºao foi reconhecida a categoria\n");
+        printf("\n");
+        for (Node *product = new_head->next; product != NULL; product = product->next) {
+            Product *data = (Product *)product->data;
+            printf("ID do produto: %d, Categoria: %s, Preço: %.2f\n", data->id, data->category, data->price);
+        }
+    } else {
+        for (Node *product = new_head->next; product != NULL; product = product->next) {
+            Product *data = (Product *)product->data;
+            if(strcmp(data->category,input_category) == 0){
+                printf("ID do produto: %d, Categoria: %s, Preço: %.2f\n", data->id, data->category, data->price);
+            }
+        }
+    }
+    printf("\n");
+
+    //TODO(), falta fazer o sort
 }
 
 
+
 int main(){
+    Users *users = user_get();
+    Products *products = products_get();
+    Cart *cart = create_cart(5);
+    printf("//-------------------------------------------------------//\n");
+    printf("Now you can start\n");
 	char line[100];
 	while (1) {
 		if (fgets(line, sizeof(line), stdin) == NULL)
@@ -60,15 +101,14 @@ int main(){
 		switch (command[0]) {
 			case 'U':
             case 'u':
-                list_users();
+                list_users(users);
                 break;
-            /*
 			case 'T':
             case 't':
                 if(first_option == NULL){
                     printf("Erro: Tem de indicar o id\n");
                 } else { 
-				    identify_user(first_option);
+				    identify_user(first_option, cart);
                 }
 				break;
 			case 'P':
@@ -76,9 +116,10 @@ int main(){
                 if(first_option == NULL || second_option == NULL){
                     printf("Erro: Tem de user o formato Produtos <categoria> <critério>.\n");
                 } else {
-                    list_specified_products(first_option, second_option);
+                    list_specified_products(first_option, second_option, products);
                 }
 				break;
+            /*
 			case 'C':
             case 'c':
                 list_products_in_cart();
@@ -100,6 +141,7 @@ int main(){
             case 'E':
             case 'e':
                 printf("Bye Byee!\n");
+                free_cart(cart);
                 exit(0);
                 break;
             default:
@@ -107,5 +149,6 @@ int main(){
                 break;
 		}
 	}
+    free_cart(cart);
     return 0;
 }
